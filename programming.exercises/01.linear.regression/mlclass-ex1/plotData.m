55945785a59c129a30d950207c8722d333b05fd3
Anderson Mesquita <andersonvom@gmail.com>
2011-10-25 13:41:31 -0400

    ML: Completed plot data function

--- programming.exercises/01.linear.regression/mlclass-ex1/plotData.m
+++ programming.exercises/01.linear.regression/mlclass-ex1/plotData.m
@@ -15,9 +15,9 @@ function plotData(x, y)
 %       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
 
 figure; % open a new figure window
-
-
-
+plot(x, y, 'rx', 'MarkerSize', 10);		% Plot the data
+ylabel('Profit in $10,000s');			% Set the y-axis label
+xlabel('Population of City in $10,000s');	% Set the x-axis label
 
 
 

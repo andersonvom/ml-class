27538e6114684a808926405e459dc19118c7aba5
Anderson Mesquita <andersonvom@gmail.com>
2011-10-25 15:34:46 -0400

    ML: Completed computeCost function

--- programming.exercises/01.linear.regression/mlclass-ex1/computeCost.m
+++ programming.exercises/01.linear.regression/mlclass-ex1/computeCost.m
@@ -13,9 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+for i = 1:m
+	J = J + ( ( theta' * X(i,:)' ) - y(i) )^2
+endfor
+J = J / (2*m)
 
 % =========================================================================
 

5fa155505268459cdf6229c0e3bcabb4721a889e
Anderson Mesquita <andersonvom@gmail.com>
2011-10-25 22:07:42 -0400

    ML: refactored cost function to make use of matrix operations

--- programming.exercises/01.linear.regression/mlclass-ex1/computeCost.m
+++ programming.exercises/01.linear.regression/mlclass-ex1/computeCost.m
@@ -13,10 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-for i = 1:m
-	J = J + ( ( theta' * X(i,:)' ) - y(i) )^2;
-endfor
-J = J / (2*m);
+Theta = (theta * ones(1,m))';		% m tuples of theta
+h_theta = sum( Theta .* X, 2 );		% theta0 * x0 + theta1 * x1
+squared_sum = sum( (h_theta - y).^2 );
+J = 1/(2*m) * squared_sum;
 
 % =========================================================================
 

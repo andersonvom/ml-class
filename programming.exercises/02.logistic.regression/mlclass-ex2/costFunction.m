5a3e113916e10af68e1b88615d6bb80e30fc240a
Anderson Mesquita <andersonvom@gmail.com>
2011-10-30 19:05:36 -0400

    ML: Implemented cost function and gradient computation for logistic regression

--- programming.exercises/02.logistic.regression/mlclass-ex2/costFunction.m
+++ programming.exercises/02.logistic.regression/mlclass-ex2/costFunction.m
@@ -20,11 +20,11 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+z = X*theta;
+h_x = sigmoid(z);
+J = 1/m * sum( -y .* log(h_x) - (1-y) .* log(1 - h_x) );
 
-
-
-
-
+grad = (1/m) * ((h_x - y)' * X)';
 
 
 % =============================================================

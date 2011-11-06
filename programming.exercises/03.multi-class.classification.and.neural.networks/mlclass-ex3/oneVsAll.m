f89baf17d6d30995c195d0151400a71384820e27
Anderson Mesquita <andersonvom@gmail.com>
2011-11-05 20:00:24 -0400

    ML: Implemented one-vs-all classification neurons

--- programming.exercises/03.multi-class.classification.and.neural.networks/mlclass-ex3/oneVsAll.m
+++ programming.exercises/03.multi-class.classification.and.neural.networks/mlclass-ex3/oneVsAll.m
@@ -50,12 +50,13 @@ X = [ones(m, 1) X];
 %
 
 
+initial_theta = zeros(n + 1, 1);
+options = optimset('GradObj', 'on', 'MaxIter', 50);
 
-
-
-
-
-
+for c=1:num_labels
+	theta_i = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
+	all_theta(c,:) = theta_i(:);
+endfor
 
 
 

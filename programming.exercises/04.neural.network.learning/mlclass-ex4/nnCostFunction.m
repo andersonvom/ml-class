ad7858fe74a4fd6d2c5a8e49b78bbcabbe424e0a
Anderson Mesquita <andersonvom@gmail.com>
2011-11-13 14:30:55 -0500

    ML: Refactored variable name for clarity and consistency

--- programming.exercises/04.neural.network.learning/mlclass-ex4/nnCostFunction.m
+++ programming.exercises/04.neural.network.learning/mlclass-ex4/nnCostFunction.m
@@ -72,14 +72,14 @@ y = y2;
 
 
 % Forward Propagation - BEGIN
-X = [ones(m,1) X];
-z2 = X * Theta1';
+a1 = [ones(m,1) X];
+z2 = a1 * Theta1';
 a2 = sigmoid(z2);
 
 a2 = [ones(size(a2,1),1) a2];
 z3 = a2 * Theta2';
 a3 = sigmoid(z3);
-h_x = a3;						% for clarity only
+h_x = a3; % for clarity only
 % Forward Propagation - END
 
 

acece4b8e4cadfec71efbb64ffc62a11f9dced82
Anderson Mesquita <andersonvom@gmail.com>
2011-12-13 21:12:33 -0500

    ML: Refactored cost function and added regularization to gradients in collaborative filtering

--- programming.exercises/08.anomaly.detection.and.recommender.systems/mlclass-ex8/cofiCostFunc.m
+++ programming.exercises/08.anomaly.detection.and.recommender.systems/mlclass-ex8/cofiCostFunc.m
@@ -43,12 +43,12 @@ Theta_grad = zeros(size(Theta));
 diff_error = (X * Theta' - Y) .* R;	% Only compute cost when user has rated the movie
 square_error = diff_error .^ 2;
 
-J = (1/2) * sum(sum(square_error)) + (lambda/2) * ( sum(sum(Theta.^2)) + sum(sum(X.^2)) );
+J = (1/2) * sum(sum(square_error)) + (lambda/2) * ( sum(Theta(:).^2) + sum(X(:).^2) );
 
 
 % Gradients for X and Theta
-X_grad = diff_error * Theta;
-Theta_grad = diff_error' * X;
+X_grad = diff_error * Theta + lambda * X;
+Theta_grad = diff_error' * X + lambda * Theta;
 
 
 
